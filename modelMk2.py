1eff5a6d2d845d576b28153eda132dcdc7530308
Alessandro Stefani <alessandro.stefani@omnicon.it>
2023-11-24 10:59:55 +0100

    fatti un po di test con i parametri, al momento quelli presenti riultano i migliori. considero di fare una resample a 30 minuti e provare il modello

--- modelMk2.py
+++ modelMk2.py
@@ -15,22 +15,15 @@ from skforecast.model_selection import grid_search_forecaster
 DATE_LIMITER = "2023-08-01 00:00:00"
 TRAIN_SET_PERC = 0.8
 LAGS = [96, 192, 288, 384, 480] # 1,2,3,4,5 giorni
-STEPS = 4 # 1h
+STEPS = 5 #1h
+lags = LAGS[0]
 
 # FUNC
 
-def trainForecasters(regressors, lags, trSet):
-    models = []
-    for reg in regressors:
-        for lag in lags:
-            m = ForecasterAutoreg(regressor=reg, lags=lag)
-            start=time.time()
-            m.fit(trSet["Value"])
-            end= time.time()
-            ttime = end - start
-            models.append([m, reg, lag ,ttime])
-            print(f" {len(models)}/{len(regressors)*len(lags)}", end="\r")
-    return pd.DataFrame(data=models, columns=["Model", "Regressor", "Lag", "Training Time"])
+def trainForecaster(lags, steps, reg):
+    model = 0
+    
+    return model    
 
 def predict(setToPredict ,lags, steps):
     i=0
@@ -41,8 +34,8 @@ def predict(setToPredict ,lags, steps):
         print(f"step {i+1}/{lenVal - lags}", end="\r")
         batch = setToPredict.iloc[i:i + lags]["Value"]
         betterPred = model.predict(steps , last_window=batch)
-        newPreds.append(betterPred.iloc[-1])
-        newTimes.append(betterPred.index[-1])
+        newPreds += [betterPred.iloc[-1]]
+        newTimes += [betterPred.index[-1]]
         i += 1
     pred = pd.Series(newPreds, newTimes)
     print("\n")
@@ -52,7 +45,6 @@ def printScore(realData, predictedData):
     mape = mean_absolute_percentage_error(realData, predictedData)
     r2 = r2_score(realData, predictedData)
     txt = f"SCORE\nmape: {mape:.5}\nr2: {r2:.5}"
-    plt.text(0.5, 0.5, txt)
     print("___SCORE___")
     print(f"mape: {mape:.5}")
     print(f"r2: {r2:.5}")
@@ -95,18 +87,8 @@ print(strangeData)
 
 # MODEL TRAINING
 
-# xgb = XGBRegressor()
-# rf = RandomForestRegressor()
-# regs = [xgb]
-
-# fs = trainForecasters(regs, LAGS, trainingSet)
-
-# print("\nTRAINED MODELS:")
-# print(fs)
-
 #base
 xgb = XGBRegressor()
-lags = LAGS[0]
 
 model = ForecasterAutoreg(regressor=xgb, lags=lags)
 
@@ -115,31 +97,11 @@ model.fit(trainingSet["Value"])
 end= time.time()
 print(f"\nTRAINING TIME: {(end - start):.3} s")
 
-#aaa
-# xgb = XGBRegressor()
-# rf = RandomForestRegressor()
-# lags = 24*4
-
-# model = ForecasterAutoreg(regressor=xgb, lags=lags)
-
-# results_grid = grid_search_forecaster(
-#     forecaster  = model,
-#     y           = trainingSet["Value"],
-#     param_grid  = {},
-#     lags_grid   = [LAGS[0]],
-#     steps       = 4,
-#     refit       = False,
-#     metric      = ['mean_squared_error', 'mean_absolute_error', 'mean_absolute_percentage_error', 'mean_squared_log_error'],
-#     initial_train_size = int(len(trainingSet["Value"])*0.5),
-#     return_best = True,
-#     verbose     = False
-# )
-
 # PREDIZIONI
 
 #sul training set
 start = time.time()
-predVal = predict(trainingSet, LAGS[0], STEPS)
+predVal = predict(trainingSet, lags, STEPS)
 plt.plot(predVal, label="predizione Training set")
 end = time.time()
 print(f"predizione sul Training set: {int(end-start)} s")
@@ -148,7 +110,7 @@ printScore(allData.loc[predVal.index].values, predVal.values)
 
 #sul validation set
 start = time.time()
-predVal = predict(validationSet, LAGS[0], STEPS)
+predVal = predict(validationSet, lags, STEPS)
 plt.plot(predVal, label="predizione Validation set")
 end = time.time()
 print(f"predizione sul validation set: {int(end-start)} s")
@@ -157,16 +119,13 @@ printScore(allData.loc[predVal.index].values, predVal.values)
 
 #sullo strange set
 start = time.time()
-predVal = predict(strangeData, LAGS[0], STEPS)
+predVal = predict(strangeData, lags, STEPS)
 plt.plot(predVal, label="predizione Strange set")
 end = time.time()
 print(f"predizione sulo Strange set: {int(end-start)} s")
 #evito di inserire le previsioni non presenti nei dati per fare lo score
 printScore(allData.loc[predVal.iloc[:-STEPS+1].index].values, predVal.iloc[:-STEPS+1].values)
 
-
-
-plt.text(0.5, 0.5, "Giao")
 
 plt.legend()
 plt.show()
